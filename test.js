5dd78a985c74c238fedf2689493c2839b3b7a94c
leMaik <an@maik.tk>
2018-05-23 23:28:58 +0200

    Update icons, use ava for testing.

---
+++ test.js
@@ -0,0 +1,30 @@
+import test from 'ava'
+import React from 'react'
+import renderer from 'react-test-renderer'
+import fs from 'fs'
+const commonjsIcons = require('./package/index')
+
+test('the npm package', (t) => {
+  // should set sideEffects to false to allow webpack to optimize re-exports
+  const packageJson = require('./package/package.json')
+  t.false(packageJson.sideEffects)
+})
+
+for (const iconName of Object.keys(commonjsIcons)) {
+  test(`icons > ${iconName}`, (t) => {
+    const renderedIcon = renderer.create(React.createElement(commonjsIcons[iconName])).toJSON()
+    t.is(renderedIcon.type, 'svg')
+  })
+}
+test('ES module index file', (t) => {
+  const esmReExports = fs.readFileSync('./package/index.es.js', 'utf-8')
+    .split('\n')
+    .filter((line) => line.length > 0)
+  t.is(esmReExports.length, Object.keys(commonjsIcons).length)
+
+  for (const line of esmReExports) {
+    const match = line.match(/^export \{ default as (.+?) \} from '\.\/(.+?)'$/)
+    t.is(match[1], match[2])
+    t.truthy(commonjsIcons[match[1]])
+  }
+})
