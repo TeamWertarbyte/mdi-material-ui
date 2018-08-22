2a2a9becac0a18e0d67369a5455ff397c3fe4003
Maik Marschner <m.marschner@wertarbyte.com>
2018-08-22 18:58:48 +0200

    Update storybook.

--- stories/index.js
+++ stories/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { storiesOf } from '@kadira/storybook'
+import { storiesOf } from '@storybook/react'
 const icons = require('../package/index')
 
 function themed (children) {
