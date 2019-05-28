ba90ad7b8dac91df7a456f2bd84be6e9af5ce6bd
Maik Marschner <an@maik.tk>
2019-05-28 21:02:45 +0200

    Use React.useMemo and React.useForwardRef. Bump the React peer dependency.

--- src/util/createIcon.js
+++ src/util/createIcon.js
@@ -2,7 +2,13 @@ import React from 'react'
 import SvgIcon from '@material-ui/core/SvgIcon'
 
 export default (path) => {
-  const Icon = (props) => <SvgIcon {...props}><path d={path} /></SvgIcon>
+  const Icon = React.memo(
+    React.forwardRef((props, ref) => (
+      <SvgIcon {...props} ref={ref}>
+        <path d={path} />
+      </SvgIcon>
+    ))
+  )
   Icon.muiName = 'SvgIcon'
   return Icon
 }
