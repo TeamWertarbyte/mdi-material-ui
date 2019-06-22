16fc2a042afa61f24a1a05858832c52c7e006705
Maik Marschner <an@maik.tk>
2019-06-22 23:13:52 +0200

    Add the Material Design Light icons. Update Material Design Icons.

--- stories/index.js
+++ stories/index.js
@@ -4,6 +4,7 @@ import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import getMuiTheme from 'material-ui/styles/getMuiTheme'
 const icons = require('../package/index')
+const lightIcons = require('../package/light/index')
 
 function themed (children) {
   return (
@@ -15,7 +16,7 @@ function themed (children) {
   )
 }
 
-const iconStories = storiesOf('Icons', module)
+const iconStories = storiesOf('Material Design Icons', module)
 iconStories.add(`all icons (${Object.keys(icons).length.toLocaleString()})`, () => themed(
   <div>
     {Object.keys(icons).map((icon) => {
@@ -28,4 +29,19 @@ iconStories.add(`all icons (${Object.keys(icons).length.toLocaleString()})`, () 
 Object.keys(icons).sort().forEach((icon) => {
   const Icon = icons[icon]
   iconStories.add(icon, () => themed(<Icon />))
+})
+
+const lightIconStories = storiesOf('Material Design Icons Light', module)
+lightIconStories.add(`all icons (${Object.keys(lightIcons).length.toLocaleString()})`, () => themed(
+  <div>
+    {Object.keys(lightIcons).map((icon) => {
+      const Icon = lightIcons[icon]
+      return <Icon key={icon} />
+    })}
+  </div>
+))
+
+Object.keys(lightIcons).sort().forEach((icon) => {
+  const Icon = lightIcons[icon]
+  lightIconStories.add(icon, () => themed(<Icon />))
 })
--- test.js
+++ test.js
@@ -4,6 +4,7 @@ import renderer from 'react-test-renderer'
 import fs from 'fs'
 import { MuiThemeProvider } from 'material-ui/styles';
 const commonjsIcons = require('./package/index')
+const commonjsIconsLight = require('./package/light/index')
 
 test('the npm package', (t) => {
   // should set sideEffects to false to allow webpack to optimize re-exports
@@ -22,6 +23,7 @@ for (const iconName of Object.keys(commonjsIcons)) {
     t.is(commonjsIcons[iconName].muiName, 'SvgIcon')
   })
 }
+
 test('ES module index file', (t) => {
   const esmReExports = fs.readFileSync('./package/index.es.js', 'utf-8')
     .split('\n')
@@ -32,5 +34,26 @@ test('ES module index file', (t) => {
     const match = line.match(/^export \{ default as (.+?) \} from '\.\/(.+?)'$/)
     t.is(match[1], match[2])
     t.truthy(commonjsIcons[match[1]])
+  }
+})
+
+for (const iconName of Object.keys(commonjsIconsLight)) {
+  test(`light icons > ${iconName}`, (t) => {
+    const renderedIcon = renderer.create(React.createElement(commonjsIconsLight[iconName])).toJSON()
+    t.is(renderedIcon.type, 'svg')
+    t.is(commonjsIconsLight[iconName].muiName, 'SvgIcon')
+  })
+}
+
+test('mdi-light ES module index file', (t) => {
+  const esmReExports = fs.readFileSync('./package/light/index.es.js', 'utf-8')
+    .split('\n')
+    .filter((line) => line.length > 0)
+  t.is(esmReExports.length, Object.keys(commonjsIconsLight).length)
+
+  for (const line of esmReExports) {
+    const match = line.match(/^export \{ default as (.+?) \} from '\.\/(.+?)'$/)
+    t.is(match[1], match[2])
+    t.truthy(commonjsIconsLight[match[1]])
   }
 })
