0d2380d5d05bd4cfdce595e06e1ef35d439ccfa7
Maik Marschner <an@maik.tk>
2019-08-14 21:24:51 +0200

    Update to mdi 4.0.96.

--- update.sh
+++ update.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 npm i --save-dev @mdi/js@latest @mdi/light-js@latest
 ./generate-module.js
+
+VERSION=`cat node_modules/@mdi/js/package.json | jq -r .version`
+git add --all
+git commit -m "Update to mdi $VERSION."
+git push origin master
