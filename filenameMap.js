1030472a1c505c5e4db4c8f3304943397a01c9c7
Maik Marschner <m.marschner@wertarbyte.com>
2021-04-23 17:39:19 +0200

    Remove the TextBox icon from the filename map, the Textbox icon has been renamed.

--- filenameMap.js
+++ filenameMap.js
@@ -1,6 +1,1 @@
-// There are icons names that, when converted to PascalCase, only differ in casing. This is
-// problematic on file systems that are not case-sensitive.
-// See TeamWertarbyte/mdi-material-ui#30 for more information
-module.exports = {
-  TextBox: 'Text-Box' // textbox vs. text-box
-}
+module.exports = {};
