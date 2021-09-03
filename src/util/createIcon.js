390b4e18e5c5fed519581f05bdeef82458c11a3d
ShotSkydiver <bigglesworth330@gmail.com>
2021-09-02 19:34:55 -0600

    Make necessary changes to update to material-ui v5 RC

--- src/util/createIcon.js
+++ src/util/createIcon.js
@@ -1,4 +1,4 @@
 import * as React from "react";
-import { createSvgIcon } from "@material-ui/core/utils";
+import { createSvgIcon } from "@mui/material/utils";
 
 export default (path, name) => createSvgIcon(<path d={path} />, name);
